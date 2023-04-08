5a0dede65d35e3577bc6a42cf768870b52f0b2aa
yejin <yejin213527@gmail.com>
2023-04-08 16:39:35 +0900

    String

---
+++ Test/test03.py
@@ -0,0 +1,9 @@
+# 문자열 String
+# 문자형태의 데이터 " 또는 '로 문자열의 시작, 끝을 나타냄
+"this is string"
+'this is string'
+
+"He thinks, 'I am hungry...'"
+
+'He thinks, \'I am hungry...\''  # escape 문자
+"I want to push ENTER.\n"  # 줄바꿈
